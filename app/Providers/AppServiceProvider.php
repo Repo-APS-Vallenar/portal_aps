52d5aeced405579309692214e7a9613fa12e9b97
Desarrollo Valle Conecta <informatica.aps.vallenar@gmail.com>
2025-06-05 08:39:59 -0400

    appservice

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,11 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (env('APP_ENV') !== 'local') {
-            URL::forceScheme('http');
-        }
         if (app()->environment('production')) {
             URL::forceScheme('https');
+        } else {
+            URL::forceScheme('http');
         }
     }
 }
